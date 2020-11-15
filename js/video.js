312f566aca87039d63734cdd8de8e6c116c93d33
shannonli7 <lishann@umich.edu>
2020-11-15 13:05:06 -0500

    code change

--- js/video.js
+++ js/video.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	video.play();
 	console.log("Play Video");
-	video.volume = "100%";
+	video.volume.innerHTML = "100%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
